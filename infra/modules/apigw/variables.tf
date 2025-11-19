98fd811662d05c583b5979d3fda29918e44d4cd2
beyzagoknl <beyzagoknl@gmail.com>
2025-11-19 20:37:22 +0100

    added apigateway modules

---
+++ infra/modules/apigw/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type        = string
+  description = "Project name"
+}
+
+variable "lambda_arn" {
+  type        = string
+  description = "Lambda invoke ARN"
+}
+
+variable "lambda_function_name" {
+  type        = string
+  description = "Lambda function name"
+}
