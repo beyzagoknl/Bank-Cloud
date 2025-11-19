37b25c96687c48cb562983526d60d232d3e5b891
beyzagoknl <beyzagoknl@gmail.com>
2025-11-19 20:32:39 +0100

    added lambda module

---
+++ infra/modules/lambda/main.tf
@@ -0,0 +1,69 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_cloudwatch_log_group" "lambda" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = 400 # ~13 months
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.project_name}-${var.function_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = {
+    Project = var.project_name
+  }
+}
+
+# Basic logging + VPC access
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = var.function_name
+  role          = aws_iam_role.lambda_exec.arn
+  runtime       = var.runtime
+  handler       = var.handler
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
+  }
+
+  environment {
+    variables = var.environment
+  }
+
+  depends_on = [aws_cloudwatch_log_group.lambda]
+
+  tags = {
+    Project = var.project_name
+  }
+}
